5ec92a7894e6a89691419f6208cd1b7b97e035bc
GMarx90 <57858033+GMarx90@users.noreply.github.com>
2020-02-11 17:47:22 +0100

    Update AppStore.java

--- StoreApp Java/AppStore.java
+++ StoreApp Java/AppStore.java
@@ -1,7 +1,11 @@
 
-public class GiftShop {
+public class AppStore {
     public static void main(String[] arguments) {
         Store store = new Store();
+        store.addItem("C01", "KUBEK", "9.99", "150", "NIE");
+        store.addItem("C02", "DUŻY KUBEK", "12.99", "82", "TAK");
+        store.addItem("C03", "PODKŁADKA", "10.49", "800", "NIE");
+        store.addItem("D01", "KOSZULKA", "16.99", "90", "TAK");
         store.addItem("C01", "KUBEK", "9.99", "150", "NIE");
         store.addItem("C02", "DUŻY KUBEK", "12.99", "82", "TAK");
         store.addItem("C03", "PODKŁADKA", "10.49", "800", "NIE");
@@ -17,7 +21,7 @@ public class GiftShop {
         store.sort();
 
         for (int i = 0; i < store.getSize(); i++) {
-            Item show = store.getItem(i);
+            Item show = (Item)store.getItem(i);
             System.out.println("\nIdentyfikator elementu: " + show.getId() +
                     "\nNazwa: " + show.getName() +
                     "\nCena sprzedaży: " + show.getRetail() + " zł" +
@@ -26,3 +30,4 @@ public class GiftShop {
         }
     }
 }
+
