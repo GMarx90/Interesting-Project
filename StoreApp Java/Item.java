71a6862d33c5ce59467526fb0c61580dc69b0402
GMarx90 <57858033+GMarx90@users.noreply.github.com>
2020-02-11 17:15:29 +0100

    Update Item.java

--- StoreApp Java/Item.java
+++ StoreApp Java/Item.java
@@ -1,34 +1,35 @@
-public class Item implemets Comparable{
-    private Intiger id;
+
+
+public class Item implements Comparable{
+    private Integer id;
     private String name;
-    private String retail; 
+    private double retail;
     private int quantity;
     private double price;
     private boolean noDiscount;
-  Item(Intiger id, String name, String retail, String quantity, boolean noDiscount) {
-    this.id = id;
-    this.name = name;
-    this.retail = Double.parseDouble(retail);
-    this.quantity = Integer.parseInt(quantity);
-    this.noDicount=npDicount.equlas("YES");
+    Item(Integer id, String name, double retail, int quantity, boolean noDiscount) {
+        this.id = id;
+        this.name = name;
+        this.retail = retail;
+        this.quantity = quantity;
+        this.noDiscount=noDiscount;
 
 
-if (noDiscount){
-        price=retail;
+        if (noDiscount)
+            price = retail;
+        else{
+            if (quantity >= 100){
+                price=0.5D*retail;
+                price = Math.round(price);
+            }
+            else if(quantity < 100&& quantity >25){
+                price=0.75D*retail;
+                price = Math.round(price);
+            }}
+
+        //     price = Math.floor( price * 100 + .5 ) / 100;
+
     }
-else{
-    if (quantity >= 100){
-        price=0.5D*retail;
-        price = Math.round(price);
-        }
-    else if(quantity < 100&& quantity >25){
-        price=0.75D*retail;
-        price = Math.round(price);
-    }}
-
-    //     price = Math.floor( price * 100 + .5 ) / 100;
-    
-}
     public int compareTo(Object obj) {        //sortowanie po cenie
         Item temp = (Item) obj;
         if (this.price < temp.price) {
@@ -39,7 +40,7 @@ else{
         return 0;
     }
 
-    public String getId() {
+    public Integer getId() {
         return id;
     }
 
